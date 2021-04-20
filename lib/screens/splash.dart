328262a66ff0036a9298a7f7f20de05b72f624e6
Rodger Kumwanje <rkumwanje@gmail.com>
2021-04-20 19:37:51 +0000

    pub packages

---
+++ lib/screens/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hcr/constants/global.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Future wait() async {
+    await Future.delayed(Duration(seconds: 4),
+        () => {Navigator.pushReplacementNamed(context, '/login')});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    wait();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text(
+        'HCR',
+        style: TextStyle(
+            color: textColor,
+            fontFamily: 'Avenir',
+            fontWeight: FontWeight.bold,
+            fontSize: 40),
+      ),
+    ));
+  }
+}
